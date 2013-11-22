5a82e1c4564102587282507896edc56fbcdd221a
Lettirydl <lettierydl@gmail.com>
2013-11-21 22:52:50 -0300

    FinderVenda alterado
    Implantado outras consultas no FinderVenda

--- src/com/twol/sigep/model/vendas/FinderVenda.java
+++ src/com/twol/sigep/model/vendas/FinderVenda.java
@@ -1,124 +1,81 @@
 package com.twol.sigep.model.vendas;
 
-import java.util.HashMap;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.persistence.Query;
 
+import com.twol.sigep.model.pessoas.Cliente;
+import com.twol.sigep.model.pessoas.Dependente;
 import com.twol.sigep.model.pessoas.Funcionario;
 import com.twol.sigep.util.Persistencia;
 
 public class FinderVenda {
 
 	@SuppressWarnings("unchecked")
-	public static List<Venda> VendasQueNomeLike(
-			String nome) {
-		if (nome == null || nome.length() == 0)
+	public static List<Venda> vendasDoFuncionarioEntreDias(Funcionario f, Cliente c, Dependente d, Calendar dataInicio,
+			Calendar dataFim, FormaDePagamento fpag) {
+		if (f == null) {
 			throw new IllegalArgumentException(
-					"É necessário um nome válido");
-		nome = nome.replace('*', '%');
-		if (nome.charAt(0) != '%') {
-			nome = "%" + nome;
+					"É necessário uma Funcionario válida");
 		}
-		if (nome.charAt(nome.length() - 1) != '%') {
-			nome += "%";
+		String consulta = "select v from Venda v where v.funcionario = :func";
+		Map<String, Object> parametros = new TreeMap<String, Object>();
+		parametros.put("func", f);
+		if (dataInicio != null && dataFim != null) {
+			parametros.put("dataInicio", dataInicio);
+			parametros.put("dataFim", dataFim);
+			consulta += " and v.dia between :dataInicio and :dataFim";
+		} else if (dataInicio != null) {
+			parametros.put("dataInicio", dataInicio);
+			consulta += " and v.dia =:dataInicio";
+		} else if (dataFim != null) {
+			parametros.put("dataFim", dataFim);
+			consulta += " and v.dia =:dataFim";
 		}
-		Persistencia.em.getTransaction().begin();
-		Query q = Persistencia.em
-				.createQuery(
-						"select o from Venda as o where LOWER(o.nome) LIKE LOWER(:nome)",
-						Venda.class);
-		q.setParameter("nome", nome);
-		List<Venda> Vendas = q.getResultList();
-		Persistencia.em.getTransaction().commit();
+		if (c != null) {
+			parametros.put("cli", c);
+			consulta += " and v.cliente :cli";
+		}
+		if (d != null) {
+			parametros.put("dep", d);
+			consulta += " and v.dependente :dep";
+		}
+		if (fpag != null) {
+			parametros.put("fpag", d);
+			consulta += " and v.formaDePagamento :fpag";
+		}
+		Query query = Persistencia.em.createQuery(consulta, Venda.class);
+		for (Map.Entry<String, Object> param : parametros.entrySet()) {
+			query.setParameter(param.getKey(), param.getValue());
+		}
+		List<Venda> Vendas = (List<Venda>) query.getResultList();
+		return Vendas;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Venda> vendasDoFuncionario(Funcionario f) {
+		if (f == null)
+			throw new IllegalArgumentException(
+					"É necessário uma Funcionario válida");
+		Query query = Persistencia.em.createQuery(
+				"select v from Venda as v where v.funcionario = :func"
+				,Venda.class);
+		query.setParameter("func", f);
+		List<Venda> Vendas = (List<Venda>) query.getResultList();
 		return Vendas;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static List<Venda> VendasQueCPFLike(String cpf) {
-		if (cpf == null || cpf.length() == 0)
-			throw new IllegalArgumentException(
-					"É necessário uma CPF válido");
-		cpf = cpf.replace('*', '%');
-		if (cpf.charAt(0) != '%') {
-			cpf = "%" + cpf;
-		}
-		if (cpf.charAt(cpf.length() - 1) != '%') {
-			cpf += "%";
-		}
-		Persistencia.em.getTransaction().begin();
-		Query q = Persistencia.em
-				.createQuery(
-						"SELECT o FROM Venda AS o WHERE LOWER(o.cpf) LIKE LOWER(:cpf)",
-						Venda.class);
-		q.setParameter("cpf", cpf);
-		List<Venda> Vendas = q.getResultList();
-		Persistencia.em.getTransaction().commit();
-		return Vendas;
-	}
-
-	@SuppressWarnings("unchecked")
-	public static List<Venda> VendasQueNomeInicia(
-			String nome) {
-		if (nome == null || nome.length() == 0)
-			throw new IllegalArgumentException(
-					"É necessário um nome válido");
-		nome = nome.replace('*', '%');
-		if (nome.charAt(0) != '%') {
-			nome += "%";
-		}
-		Persistencia.em.getTransaction().begin();
-		Query q = Persistencia.em
-				.createQuery(
-						"select o from Venda as o where LOWER(o.nome) LIKE LOWER(:nome)",
-						Venda.class);
-		q.setParameter("nome", nome);
-		List<Venda> Vendas = q.getResultList();
-		Persistencia.em.getTransaction().commit();
-		return Vendas;
-	}
-
-	@SuppressWarnings("unchecked")
-	public static List<Venda> VendasQueCPFInicia(String cpf) {
-		if (cpf == null || cpf.length() == 0)
-			throw new IllegalArgumentException(
-					"É necessário uma CPF válida");
-		cpf = cpf.replace('*', '%');
-		if (cpf.charAt(0) != '%') {
-			cpf += "%";
-		}
-		Persistencia.em.getTransaction().begin();
-		Query q = Persistencia.em
-				.createQuery(
-						"select o from Venda as o where LOWER(o.cpf) LIKE LOWER(:cpf)",
-						Venda.class);
-		q.setParameter("cpf", cpf);
-		List<Venda> Vendas = q.getResultList();
-		Persistencia.em.getTransaction().commit();
-		return Vendas;
-	}
-
-	@SuppressWarnings("unchecked")
-	public static List<Venda> VendasQueDescricaoOuCodigoDeBarrasIniciam(
-			String descricao, String codigoDeBarras) {
-		String stringQuery = "select p FROM Venda as p ";
-		Map<String, String> valores = new HashMap<String, String>();
-		if (descricao != null) {
-			stringQuery += "where LOWER(p.descricao) LIKE LOWER(:descricao) ";
-			valores.put("descricao", descricao + "%");
-			if (codigoDeBarras != null) {
-				stringQuery += "and LOWER(p.codigoDeBarras) LIKE LOWER(:codigoDeBarras) ";
-				valores.put("codigoDeBarras",codigoDeBarras + "%");
-			}
-		} else if (codigoDeBarras != null) {
-			stringQuery += "where LOWER(p.codigoDeBarras) LIKE LOWER(:codigoDeBarras) ";
-			valores.put("codigoDeBarras", codigoDeBarras + "%");
-		}
-		Query query = Persistencia.em.createQuery(stringQuery, Venda.class);
-		for (Map.Entry<String, String> valor : valores.entrySet()) {
-			query.setParameter(valor.getKey(), valor.getValue());
-		}
+	public static List<Venda> vendasEntreAsDatas(
+			Calendar diaInicio, Calendar diaFim) {
+		Query query = Persistencia.em.createQuery(
+				"select v from Venda as v where v.dia between :diaInicio and :diaFim"
+				,Venda.class);
+		query.setParameter("diaInicio", diaInicio);
+		query.setParameter("diaFim", diaFim);
 		List<Venda> Vendas = (List<Venda>) query.getResultList();
 		return Vendas;
 	}
